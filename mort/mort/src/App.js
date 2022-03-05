0c02ac00790fedb624de59f4d8d71c2ebe517ca1
Bodi Augustus <bodiAugustus@protonmail.com>
2022-03-05 10:26:39 -0600

    Added new section

--- mort/mort/src/App.js
+++ mort/mort/src/App.js
@@ -1,6 +1,6 @@
 
 import './index.css';
-import { Missing, Section2, Section3, Section4, Section5, Section6, Section7, Section8, Section9, Section10, Section11, Section12, Section13, Section14, Section15, Section15b, Section16, Section17, Section18, Section19, Section20, Section21, Section22, Section23, Section24, Section25, Section26, Section27, Section28, Section29, Section30, Section31, Section32 } from './sections';
+import { Missing, Section2, Section3, Section4, Section5, Section6, Section7, Section8, Section9, Section10, Section11, Section12, Section13, Section14, Section15, Section15b, Section16, Section17, Section18, Section19, Section20, Section21, Section22, Section23, Section24, Section25, Section26, Section27, Section27b, Section28, Section29, Section30, Section31, Section32 } from './sections';
 
 function App() {
   return (
@@ -34,6 +34,7 @@ function App() {
     <Section25/>
     <Section26/>
     <Section27/>
+    <Section27b/>
     <Section28/>
     <Section31/>
     <Section29/>
---
+++ mort/mort/src/sections/section27b/index.js
@@ -0,0 +1,44 @@
+const Section28 = () => {
+    return(
+        <div className=" bg-black text-white font-economica
+        xxs:h-[140vh]
+        xs:h-[110vh]
+        sm:text-lg sm:h-[125vh]
+        md:text-2xl
+        lg:text-xl lg:h-[100vh] lg:flex lg:p-4">
+
+<div className="lg:pt-10">
+<h3 className="text-center p-2 md:w-[80%] md:mx-auto">Ashley is sick, has missed multiple periods, and lives in a single cell with 67 other women and a single toilet.</h3>
+<img src="./images/Ashley(2).png" alt="Another person confirming that letters are being sent to the same Ashley was moved to" srcset="" 
+  className="xxs:w-[95vw]  xxs:mx-auto xxs:pt-2"
+/>
+
+<h3 className="text-center p-2 md:w-[80%] md:mx-auto">She is only allowed to write lies.</h3>
+</div>
+
+<div className="">
+<img src="./images/Ashley(3).png" alt="The eight attempt to receive information" srcset="" 
+  className="xxs:w-[95vw]  xxs:mx-auto xxs:pt-2
+  lg:w-[50vw] lg:pt-4"
+/>
+
+<h3 className="text-center p-2 md:w-[80%] md:mx-auto">In less than two years Ashley already has nightmares about dying - while Sergeant Major Watcharin Silpsamosorn who was filmed raping a prisoner in the same jail as Ashley got bail a day later.</h3>
+<img src="./images/Ashley(4).png" alt="The eight attempt to receive information" srcset="" 
+  className="xxs:w-[95vw]  xxs:mx-auto xxs:pt-2
+  lg:w-[50vw]"
+/>
+
+<h3 className="text-center p-2">PLEASE - HELP ASHLEY.</h3>
+<div className="flex justify-around">
+<a href="https://www.gofundme.com/f/help-ashley-oosthuizen?member=17832103&sharetype=teams&utm_campaign=p_na+share-sheet&utm_medium=social&utm_source=whatsapp"
+className="text-center text-3xl bg-yellow-500 px-2 py-1 rounded-lg text-sky-900 hover:scale-110 transition-all active:scale-100 xxs:mt-5 xs:mt-14
+lg:mt-6"
+>DONATE NOW</a>
+
+</div>
+</div>
+</div>
+    )
+}
+
+export default Section28
