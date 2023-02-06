57d7f1583ee473f40855865c265ba47500af139b
Bodi Augustus <bodiAugustus@protonmail.com>
2023-02-06 19:24:00 +0200

    paused for now

--- mort/mort/src/App.js
+++ mort/mort/src/App.js
@@ -1,12 +1,46 @@
-
 import './index.css';
-import { Missing, Section2, Section3, Section4, Section5, Section6, Section7, Section8, Section9, Section10, Section11, Section12, Section13, Section14, Section15, Section15b, Section16, Section17, Section18, Section19, Section20, Section21, Section22, Section23, Section24, Section25, Section26, Section27, Section27b, Section28, Section29, Section30, Section31, Section32 } from './sections';
+import {
+  Missing,
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+  Section6,
+  Section7,
+  Section8,
+  Section9,
+  Section10,
+  Section11,
+  Section12,
+  Section13,
+  Section14,
+  Section15,
+  Section15b,
+  Section16,
+  Section17,
+  Section18,
+  Section19,
+  Section20,
+  Section21,
+  Section22,
+  Section23,
+  Section24,
+  Section25,
+  Section26,
+  Section27,
+  Section27b,
+  Section28,
+  Section29,
+  Section30,
+  Section31,
+  Section32,
+} from './sections';
 
 function App() {
   return (
     <div>
-
-    <Missing/>
+      <h1>Tristan loves Ashley 🌞</h1>
+      {/* <Missing/>
     <Section2/>
     <Section3/>
     <Section4/>
@@ -39,8 +73,7 @@ function App() {
     <Section31/>
     <Section29/>
     <Section30/>
-    <Section32/>
-
+    <Section32/> */}
     </div>
   );
 }
