4f0e91bd6c8b8358a7da9b3dc69336d8d9c6af97
Bodi Augustus <bodiAugustus@protonmail.com>
2023-09-05 18:30:06 -0400

    stuff

--- mort/mort/src/sections/section2/index.js
+++ mort/mort/src/sections/section2/index.js
@@ -3,8 +3,8 @@ const Section2 = () => {
     <div
       className="bg-slate-900 text-white
         xxs:h-[158vh] 
-        xs:h-[128vh]
-        sm:text-lg sm:h-[135vh]
+        xs:h-[138vh]
+        sm:text-lg sm:h-[145vh]
         md:text-2xl md:h-[145vh]
         lg:p-4 lg:flex lg:text-xl lg:h-[100vh]
         "
